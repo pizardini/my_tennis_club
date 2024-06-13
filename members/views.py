5827a83636ae09ec9e5041f6728960fac8b6315c
Pietro Zardini <pizardini@gmail.com>
2024-06-13 19:46:28 -0300

    Initial Commit2

--- members/views.py
+++ members/views.py
@@ -3,6 +3,6 @@ from django.http import HttpResponse
 from django.template import loader
 
 
-def members(Request):
+def members(Request): a
     template = loader.get_template('myfirst.html')
     return HttpResponse(template.render())
